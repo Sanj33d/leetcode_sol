9a66127058225c017886ebf2620a533db169e786
Sanj33d <144790238+Sanj33d@users.noreply.github.com>
2025-03-19 16:04:01 +0600

    Time: 101 ms (61.51%) | Memory: 28.5 MB (67.53%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,17 +1,19 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        l = 0
-        r = len(height) - 1
+        # approach1: two pointer theory (left most end, right most end)
+        ## new: Python doesn't go to the next iteration after executing any of the incremental var (like l) or decremental var (like r). Instead of it, python reads all the codes of the loop block before going to the next iteration.
+        l_idx = 0 
+        r_idx = len(height) - 1 
         max_area = 0
 
-        while l < r:
-            left = height[l]
-            right = height[r]
+        while l_idx < r_idx:
+            left = height[l_idx]
+            right = height[r_idx]
 
-            area = (r-l) * min(left, right)
+            area = (r_idx - l_idx) * min(left, right)
             max_area = max(max_area, area)
             if left < right:
-                l += 1
+                l_idx += 1
             else: 
-                r -= 1
+                r_idx -= 1
         return max_area
