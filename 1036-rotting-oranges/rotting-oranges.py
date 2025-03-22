20882303d96306e03a88eec10932febf33b5ef19
Sanj33d <144790238+Sanj33d@users.noreply.github.com>
2025-03-22 19:52:36 +0600

    Time: 0 ms (100.00%) | Memory: 17.9 MB (27.30%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,25 +1,30 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        time = 0
-        fresh = 0
+        # approach: bfs instead of dfs because initially there can be multiple rotten oranges which will rot fresh oranges at the same time
+        ## since bfs, so using queue
+        ### the loop will function till the moment when both fresh and rotten oranges exist
+        time = 0 # ans
+        fresh = 0 # num of fresh oranges
         rows = len(grid)
         cols = len(grid[0])
+        
         q = []
+        # appending rotten oranges in queue and counting the num of fresh oranges
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 2:
                     q.append([r,c])
                 if grid[r][c] == 1:
                     fresh += 1
-        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+        directions = [[1,0],[-1,0],[0,1],[0,-1]] # 4 directional tracker
         while fresh > 0 and len(q) > 0:
             for rotten in range(len(q)):
-                r, c = q.pop(0)
+                r, c = q.pop(0) # pop(0) cuz it is q, so first one will be popped
                 for x, y in directions:
                     row = r + x
                     col = c + y
 
-                    # checking in bound and fresh
+                    # checking if (in bound) and (fresh)
                     if (0<=row<rows) and (0<=col<cols) and (grid[row][col]==1):
                         grid[row][col] = 2
                         q.append([row, col])
