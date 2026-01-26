e743a3b70a23b08145db2e097fe0debf28326f58
Sanj33d <144790238+Sanj33d@users.noreply.github.com>
2026-01-26 14:40:48 +0600

    Time: 0 ms (100.00%) | Memory: 48.4 MB (62.05%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,21 +1,30 @@
 class Solution {
+    public static int binsrch(int [] nums, int target, int left, int right){
+        int mid = left + (right - left) / 2;
+        // base case 1
+        if (left > right){ 
+            return -1;
+        }
+        // base case 2
+        if (nums[mid] == target){
+            return mid;
+        }
+        // recursive case 
+        else {
+            if (nums[mid] < target){
+                return binsrch(nums, target, mid + 1, right);
+            }
+            else{
+                return binsrch(nums, target, left, mid - 1);
+            }
+        }
+    }
+
     public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-        int mid = (left + right) / 2;
-
-        while(left<=right){
-            if(nums[mid] < target){
-                left = mid + 1;
-            }
-            else if(nums[mid] > target){
-                right = mid - 1;
-            }
-            else{
-                return mid;
-            }
-            mid = (left + right) / 2;
-        }
-        return -1;
+        
+        int ans = binsrch(nums, target, left, right);
+        return ans;
     }
 }
