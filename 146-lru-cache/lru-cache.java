165f8215439be50560602acf6dd71fa0c5232239
Sanj33d <144790238+Sanj33d@users.noreply.github.com>
2026-02-21 08:00:26 +0600

    Time: 881 ms (5.01%) | Memory: 134.4 MB (6.90%) - LeetSync

---
+++ 146-lru-cache/lru-cache.java
@@ -0,0 +1,50 @@
+class LRUCache {
+    int capacity;
+    HashMap<Integer, Integer> cache;
+    ArrayList<Integer> stack;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cache = new HashMap<>();
+        this.stack = new ArrayList<>();
+    }
+    
+    public int get(int key) {
+        if (!cache.containsKey(key)){
+            return -1;
+        }
+        else {
+            stack.remove(Integer.valueOf(key));
+            stack.add(key);
+            return cache.get(key);
+        }
+    }
+    
+    public void put(int key, int value) {
+        if (cache.containsKey(key)){
+            cache.put(key, value);
+            stack.remove(Integer.valueOf(key));
+            stack.add(key);
+        }
+        else{
+            if (cache.size() < capacity){
+                cache.put(key, value);
+                stack.add(key);
+            }
+            else{
+                int lru_key = stack.remove(0);
+                cache.remove(lru_key);
+
+                cache.put(key, value);
+                stack.add(key);
+            }
+        }
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
